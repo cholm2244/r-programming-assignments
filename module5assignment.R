a47f8168ab8620ff01745dd680806d41f282c12a
cholm2244 <cholm@usf.edu>
2025-09-28 15:37:39 -0400

    Add files via upload

---
+++ module5assignment.R
@@ -0,0 +1,21 @@
+A <- matrix(1:100,  nrow = 10)
+B <- matrix(1:1000, nrow = 10)
+
+dim(A)
+dim(B)
+
+invA <- solve(A)
+
+invA
+
+detA <- det(A)
+
+detA
+
+invB <- tryCatch(solve(B), error = function(e) e)
+
+invB
+
+detB <- tryCatch(det(B),   error = function(e) e)
+
+detB
